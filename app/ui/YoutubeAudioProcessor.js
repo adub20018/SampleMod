17cd47aa4d6999c5d815cbfa67b0310bbc244efc
adub20018 <alex.w.malone128@gmail.com>
2024-05-22 14:35:01 +1200

    update input page ui

--- app/ui/YoutubeAudioProcessor.js
+++ app/ui/YoutubeAudioProcessor.js
@@ -36,20 +36,43 @@ const YoutubeAudioProcessor = () => {
   return (
     <div>
       {!audioUrl && (
-        <form className={styles.inputForm}>
-          {/* youtube link input */}
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            placeholder="Paste YouTube Link Here"
-            className={styles.input}
-          />
-          {/*  youtube link submit button */}
-          <button className={styles.submitBtn} onClick={handleConvert}>
-            Extract Audio
-          </button>
-        </form>
+        <div className={styles.formContainer}>
+          <label className={styles.inputInstructions}>Paste YouTube Link</label>
+          <form className={styles.inputForm}>
+            {/* youtube link input */}
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder="Example: https://www.youtube.com/watch?v=qLrnkK2YEcE"
+              className={styles.input}
+            />
+            {/*  youtube link submit button */}
+            <button className={styles.submitBtn} onClick={handleConvert}>
+              Submit
+            </button>
+          </form>
+          <div className={styles.introductionContainer}>
+            <h2 className={styles.introduction}>
+              Youtube Audio Sample Modifier
+            </h2>
+            <div className={styles.instructionContainer}>
+              <ol className={styles.instructionList}>
+                <li className={styles.instruction}>
+                  Find a Youtube song you want to modify
+                </li>
+                <li className={styles.instruction}>Copy Youtube video URL</li>
+                <li className={styles.instruction}>
+                  Paste URL in the above input field.
+                </li>
+                <li className={styles.instruction}>Click Submit and wait!</li>
+                {/* <li></li>
+              <li></li>
+              <li></li> */}
+              </ol>
+            </div>
+          </div>
+        </div>
       )}
 
       {
