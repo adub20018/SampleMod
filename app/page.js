47ffd3e463779301c62c33a3ca4d9c9c73ae67af
adub20018 <alex.w.malone128@gmail.com>
2024-05-22 17:52:40 +1200

    added logo and minor ui updates

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import YoutubeAudioProcessor from "./ui/YoutubeAudioProcessor";
+import Link from "next/link";
+import logo from "../public/SampleModLogo.png";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.title}>SampleMod</h1>
+        <Image className={styles.logo} src={logo} alt="SampleMod Logo" />
+        <a className={styles.title} href={"/"}>
+          <h1>SampleMod</h1>
+        </a>
         <YoutubeAudioProcessor />
       </div>
     </main>
